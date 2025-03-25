f9f2639dc760109517823f8401fe12b903f4c1a0
zwsq <abolemail@gmail.com>
2025-03-25 05:04:59 +0330

    Log restart counts of docker containers. buggy :*

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatalf("Error creating Docker client: %v", err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+	if err != nil {
+		log.Fatalf("Error fetching containers: %v", err)
+	}
+
+	for _, container := range containers {
+		inspect, err := cli.ContainerInspect(context.Background(), container.ID)
+		if err != nil {
+			log.Printf("Error inspecting container %s: %v", container.ID, err)
+			continue
+		}
+
+		fmt.Printf("Container: %s (Name: %s) | Restart Count: %d\n",
+			container.ID[:12], container.Names[0], inspect.RestartCount)
+	}
 }
